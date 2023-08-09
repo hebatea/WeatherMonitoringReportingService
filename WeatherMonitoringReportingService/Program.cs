c396e3acb1f26303ac9c084590a03930aea725ac
Heba Ashour <hashour2098@gmail.com>
2023-08-09 12:57:38 +0300

    Add project files.

---
+++ WeatherMonitoringReportingService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringReportingService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
