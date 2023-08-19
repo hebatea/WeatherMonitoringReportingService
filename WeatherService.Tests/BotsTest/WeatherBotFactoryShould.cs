bbaec8b3ec8dd3de0b1d59ab2d3abc34b74ed9a6
Heba Ashour <hashour2098@gmail.com>
2023-08-19 19:00:31 +0300

    Test WeatherBotFactory

---
+++ WeatherService.Tests/BotsTest/WeatherBotFactoryShould.cs
@@ -0,0 +1,36 @@
+﻿using WeatherMonitoringReportingService;
+using WeatherMonitoringReportingService.Bots;
+using WeatherMonitoringReportingService.Data;
+using Xunit;
+using static WeatherMonitoringReportingService.Common;
+using FluentAssertions;
+
+namespace WeatherService.Tests.BotsTest
+{
+    public class WeatherBotFactoryShould
+    {
+        [Fact]
+        public void returnRainBotObjectWhenRainBotNeeded()
+        {
+            var rainBot = WeatherBotsFactory.GetWeatherBot(Bots.RainBot, new ConfigurationData());
+            Assert.NotNull(rainBot);
+            rainBot.Should().BeOfType<RainBot>();
+        }
+
+        [Fact]
+        public void returnSnowBotObjectWhenSnowBotNeeded()
+        {
+            var snowBot = WeatherBotsFactory.GetWeatherBot(Bots.SnowBot, new ConfigurationData());
+            Assert.NotNull(snowBot);
+            snowBot.Should().BeOfType<SnowBot>();
+        }
+
+        [Fact]
+        public void returnSunBotObjectWhenSunBotNeeded()
+        {
+            var sunBot = WeatherBotsFactory.GetWeatherBot(Bots.SunBot, new ConfigurationData());
+            Assert.NotNull(sunBot);
+            sunBot.Should().BeOfType<SunBot>();
+        }
+    }
+}
