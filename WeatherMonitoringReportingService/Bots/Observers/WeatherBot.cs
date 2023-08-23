ebd29b705e1995154e495256f44b4b7266a51b6d
Heba Ashour <hashour2098@gmail.com>
2023-08-23 09:16:31 +0300

    Add build and test file

--- WeatherMonitoringReportingService/Bots/Observers/WeatherBot.cs
+++ WeatherMonitoringReportingService/Bots/Observers/WeatherBot.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherMonitoringReportingService.Bots.Observers;
 using WeatherMonitoringReportingService.Data;
 
 namespace WeatherMonitoringReportingService.Bots
 {
-    public abstract class WeatherBot
+    public abstract class WeatherBot : IWeatherBot
     {
         protected IWeatherStrategy _strategy;
         protected ConfigurationData _configuration;
